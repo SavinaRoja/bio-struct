9e99881794fb0f653c9a997cc7e740dc6957f129
SavinaRoja <pablo.barton@gmail.com>
2012-10-26 17:51:31 -0400

    commenting out 1xki

--- tlc-analyze/main.py
+++ tlc-analyze/main.py
@@ -25,10 +25,10 @@ def main():
     
     
     
-    with open('./processed/1XKI_hyperedges.txt', 'w') as out:
-        for he in get_hyperedges('./data/1XKI.pdb', chain_keys=['A']):
-            out.write(he.__str__())
-            out.write('\n')
+    #with open('./processed/1XKI_hyperedges.txt', 'w') as out:
+    #    for he in get_hyperedges('./data/1XKI.pdb', chain_keys=['A']):
+    #        out.write(he.__str__())
+    #        out.write('\n')
 
 if __name__ == '__main__':
     print('running main')

58ce2db34023dc25918223aa58048162d0b7eeb7
Paul Barton <pablo.barton@gmail.com>
2012-11-20 17:32:13 -0500

    big improvements

--- tlc-analyze/hyperpymol.py
+++ tlc-analyze/hyperpymol.py
@@ -3,7 +3,6 @@ A small module to facilitate the interface of PyMOL and my hypergraph code. It
 will generally be used from the PyMOL interactive prompt. 
 '''
 
-from pymol import cmd
 import hypergraph
 import os.path
 from collections import namedtuple
@@ -43,30 +42,23 @@ def get_struct_hypergraph(struct_name, load_struct=True):
         cmd.center('A')
     return h
 
-def get_color_rgb(color):
-    colors = cmd.get_color_indices()
-    for c in colors:
-        if c[0] == color:
-            return cmd.get_color_tuple(colors.index(c))
-    else:
-        print('Could not find a reference for that color')
-        return None
 
-def heat_map(seq_scores, start_color='black', end_color='white', offset=0):
-    '''
-    Generates a heat map of the hyperconservation score by residues in PyMOL
-    '''
-    start_rgb = get_color_rgb(start_color)
-    end_rgb = get_color_rgb(end_color)
-    diffs = [end_rgb[i] - start_rgb[i] for i in xrange(3)]
-    min_score = min(seq_scores)
-    max_score = max(seq_scores)
-    diff_score = max_score - min_score
-    tick = 0
-    for i in seq_scores:
-        cmd.select('heat_map', 'chain a and resi {0}'.format(i + offset))
-        norm_score = (seq_scores[i] - min_score) / diff_score
-        temp_color = [norm_score * diffs[i] + start_rgb[i] for i in xrange(3)]
-        cmd.set_color('heat_col{0}'.format(tick), temp_color)
-        cmd.color('heat_col{0}'.format(tick), 'heat_map')
+#def heat_map(seq_scores, start_color='black', end_color='white', offset=0):
+#    '''
+#    Generates a heat map of the hyperconservation score by residues in PyMOL
+#    '''
+#    start_rgb = get_color_rgb(start_color)
+#    end_rgb = get_color_rgb(end_color)
+#    diffs = [end_rgb[i] - start_rgb[i] for i in xrange(3)]
+#    min_score = min(seq_scores)
+#    max_score = max(seq_scores)
+#    diff_score = max_score - min_score
+#    tick = 0
+#    for i in seq_scores:
+#        cmd.select('heat_map', 'chain a and resi {0}'.format(i + offset))
+#        norm_score = (i - min_score) / diff_score
+#        temp_color = [norm_score * diffs[i] + start_rgb[i] for i in xrange(3)]
+#        cmd.set_color('heat_col{0}'.format(tick), temp_color)
+#        cmd.color('heat_col{0}'.format(tick), 'heat_map')
+#        tick += 1
 
